30bb127eff7b477804e796c09dc81ade194a4828
Luis Reyes <alfredluis177@gmail.com>
2025-09-24 17:09:18 -0600

    Configuración de Next.js para PWA

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "offlineCache",
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
+});
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  //Otras configuraciones de Next.js
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
